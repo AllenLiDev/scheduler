0fa1abd3e0b40ccfb8648255df81fd02bc8a157d
Allen <headhuntar@gmail.com>
2020-04-14 16:16:59 -0700

    added daylist component to use daylistitem component

---
+++ src/components/DayList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DayListItem from './DayListItem';
+
+const DayList = (props) => {
+  const dayList = props.days.map(day => {
+    return <DayListItem
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay} />
+  })
+  return (<ul>{dayList}</ul>);
+}
+
+export default DayList;
