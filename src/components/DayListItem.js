333f942c31f5ac1832825b7f17a3709f61243ca0
Allen <headhuntar@gmail.com>
2020-04-14 16:16:17 -0700

    updated daylist item to use classname lib and display proper spots available

---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./DayListItem.scss";
+import classnames from 'classnames';
+
+const formatSpots = (spots) => {
+  if(spots === 0) {
+    return "no spots remaining";
+  } else if (spots === 1){
+    return "1 spot remaining";
+  } else {
+    return `${spots} spots remaining`
+  }
+}
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": (props.selected === true),
+    "day-list__item--full": (props.spots === 0)
+  });
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
