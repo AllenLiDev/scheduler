90b9c941bd45f3d2876e72993f106c2a875fa85e
Allen <headhuntar@gmail.com>
2020-04-15 14:56:55 -0700

    added header and empty space to appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,10 @@
 import "./styles.scss";
 import React from "react";
+import Header from "./Header";
 
 const Appointment = (props) => {
   return (
-    <article className="appintment"></article>
+    <article className="appointment"></article>
   )
 }
 
