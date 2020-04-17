cb79ab8e03b86fae9e098423b0d1eb40a3501b2f
Allen <headhuntar@gmail.com>
2020-04-16 17:00:26 -0700

    added storie for form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -164,4 +165,20 @@ storiesOf("Appointment", module)
       message="Could not delete Appointment"
       onClose={action("onClose")}
     />
-  ));
+  ))
+  .add("Edit", () => (
+    <Form
+      name={"Yao"}
+      interviewers={interviewers}
+      interviewer={1}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
