2772bffa4141af0b0c5f91fc1482b613c1411691
Allen <headhuntar@gmail.com>
2020-04-16 17:14:37 -0700

    added header to appointment component and added new stories to test empty / booked appointment times

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import "./styles.scss";
 
 const Appointment = (props) => {
   return (
-    <article className="appointment">{props.time}</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
   )
 }
 
